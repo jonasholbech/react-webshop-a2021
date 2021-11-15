1813707a472138e6d05448746fba56684925b632
Jonas Holbech <jh@jonasholbech.dk>
2021-11-15 13:51:03 +0100

    layout html

--- src/App.js
+++ src/App.js
@@ -2,7 +2,41 @@ import "./App.css";
 
 function App() {
   return (
-    
+    <div className="App">
+      <nav>
+        <a href="/">Home</a>
+      </nav>
+      <section>
+        <h1>Product List</h1>
+        <section className="ProductList">
+          <article>
+            <h2>Product 1</h2>
+            <p>lorem</p>
+          </article>
+          <article>
+            <h2>Product 1</h2>
+            <p>lorem</p>
+          </article>
+          <article>
+            <h2>Product 1</h2>
+            <p>lorem</p>
+          </article>
+          <article>
+            <h2>Product 1</h2>
+            <p>lorem</p>
+          </article>
+        </section>
+      </section>
+      <aside>
+        <div>
+          <h2>Your Basket</h2>
+          <p>3 items</p>
+          <p>$420</p>
+        </div>
+        <h3>Checkout</h3>
+        <form></form>
+      </aside>
+    </div>
   );
 }
 
