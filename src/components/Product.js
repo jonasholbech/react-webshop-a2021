f6d24125a8ac79afab727b64baed0d5609270682
Jonas Holbech <jh@jonasholbech.dk>
2021-11-15 16:45:18 +0100

    inline styling

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,10 @@
 export default function Product(props) {
-  console.log(props);
   return (
-    <article>
+    <article
+      style={{
+        backgroundColor: props.soldOut ? "red" : "green",
+      }}
+    >
       <h2>{props.productdisplayname}</h2>
       <p>${props.price}</p>
     </article>
