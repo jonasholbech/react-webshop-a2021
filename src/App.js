56b5f39542deadcdd219077f37d3fec64d485294
Jonas Holbech <jh@jonasholbech.dk>
2021-11-15 14:16:47 +0100

    ex2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
 import "./App.css";
 
+function Nav() {
+  return (
+    <nav>
+      <a href="/">Home</a>
+    </nav>
+  );
+}
+function Product() {
+  return (
+    <article>
+      <h2>Product 1</h2>
+      <p>lorem</p>
+    </article>
+  );
+}
+function ProductList() {
+  return (
+    <section>
+      <h1>Product List</h1>
+      <section className="ProductList">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </section>
+    </section>
+  );
+}
+function Basket() {
+  return (
+    <aside>
+      <div>
+        <h2>Your Basket</h2>
+        <p>3 items</p>
+        <p>$420</p>
+      </div>
+      <h3>Checkout</h3>
+      <form></form>
+    </aside>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <nav>
-        <a href="/">Home</a>
-      </nav>
-      <section>
-        <h1>Product List</h1>
-        <section className="ProductList">
-          <article>
-            <h2>Product 1</h2>
-            <p>lorem</p>
-          </article>
-          <article>
-            <h2>Product 1</h2>
-            <p>lorem</p>
-          </article>
-          <article>
-            <h2>Product 1</h2>
-            <p>lorem</p>
-          </article>
-          <article>
-            <h2>Product 1</h2>
-            <p>lorem</p>
-          </article>
-        </section>
-      </section>
-      <aside>
-        <div>
-          <h2>Your Basket</h2>
-          <p>3 items</p>
-          <p>$420</p>
-        </div>
-        <h3>Checkout</h3>
-        <form></form>
-      </aside>
+      <Nav />
+      <ProductList />
+      <Basket />
     </div>
   );
 }
