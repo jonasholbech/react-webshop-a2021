9b038eb8ada8982d2e41be69ba415cad5603a9b6
Jonas Holbech <jh@jonasholbech.dk>
2021-11-16 14:21:30 +0100

    ex10

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,11 @@
 import Product from "./Product";
 export default function ProductList(props) {
+  const mapped = props.products.map((product) => <Product {...product} />);
   return (
     <section>
       <h1>Product List</h1>
-      <section className="ProductList">
-        <Product {...props.product} />
-      </section>
+      <section className="ProductList">{mapped}</section>
     </section>
   );
 }
+//<Product {...props.product} />
